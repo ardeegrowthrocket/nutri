0dae8a1e6608b713bed82838a20f68b6dbe91172
Ardee Raranga <ardee@growth-rocket.com>
2018-07-29 01:12:58 +0800

    Added Clinical Payment Method

---
+++ app/code/local/Luemic/PurchaseOrder/Model/Method/Purchaseorder.php
@@ -0,0 +1,37 @@
+<?php
+
+class Luemic_PurchaseOrder_Model_Method_Purchaseorder extends Mage_Payment_Model_Method_Abstract
+{
+    // necessary for determining the correct settings
+    protected $_code  = 'clinical_quote';
+
+    public function canUseForCountry($country)
+    {
+        /*
+        for specific country, the flag will set up as 1
+        */
+        if($this->getConfigData('allowspecific')==1){
+            $availableCountries = explode(',', $this->getConfigData('specificcountry'));
+            if(!in_array($country, $availableCountries)){
+                return false;
+            }
+
+        }
+        if(Mage::getSingleton('customer/session')->isLoggedIn()){
+        	$customer_data=Mage::getSingleton('customer/session')->getCustomer();
+        	if($customer_data->getIsClinic()==1){
+        		return true;
+        	}else{
+        		return false;
+        	}
+        }else{
+        	return false;
+        }
+        return true;
+    }
+
+    public function getInstructions()
+    {
+        return trim($this->getConfigData('instructions'));
+    }    
+}
